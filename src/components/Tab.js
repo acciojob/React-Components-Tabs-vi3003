af1770404dc98123067b0d8ca8d64c5b71a694c7
“vi3003” <vidhijain3003@gmail.com>
2023-04-19 23:28:55 +0530

    submission

---
+++ src/components/Tab.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Tab = () => {
+  return (
+    <>
+      <div className="tabs">
+        <div className="tab" id="Home-tab">Home</div>
+        <div className="tab" id="About-tab">About</div>
+        <div className="tab" id="Features-tab">Features</div>
+        <div className='viewport'> Pages Go Here .</div>
+      </div>
+    </>
+  )
+}
+
+export default Tab
