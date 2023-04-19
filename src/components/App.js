ba2519d27b817ce0d051ba7d8c8cdd7cb6b77e87
“vi3003” <vidhijain3003@gmail.com>
2023-04-19 23:28:32 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,12 @@
+import React from 'react'
+// import Tab from './Tab'
+import "../styles/App.css"
+const App = () => {
+  return (
+    <div>
+        <Tab />
+    </div>
+  )
+}
 
+export default App
